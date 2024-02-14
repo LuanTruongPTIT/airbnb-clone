ec5a19a919e2adf6c93ebd89da73b4c2fcff3de6
Your Name <you@example.com>
2024-02-14 09:39:25 +0700

    init app with nx

---
+++ apps/hotel-app/src/app/components/Logo.tsx
@@ -0,0 +1,18 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push('/')}
+      className="hidden cursor-pointer md:block"
+      src="/images/logo.png"
+      width="100"
+      height="100"
+      alt="Logo"
+    />
+  );
+};
+
+export default Logo;
